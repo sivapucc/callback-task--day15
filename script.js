2360f9b94295f510ea5849f085ad03647cdce20c
sivapucc <124483364+sivapucc@users.noreply.github.com>
2023-05-07 08:59:47 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+var div=document.createElement("div");
+div.setAttribute("class","main");
+document.body.append(div);
+var count=10;
+var content="Happy New Year...2023"
+
+setTimeout(()=>{div.innerHTML=count
+count--;
+setTimeout(()=>{div.innerHTML=count
+    count--;
+    setTimeout(()=>{div.innerHTML=count
+        count--;
+        setTimeout(()=>{div.innerHTML=count
+            count--;
+            setTimeout(()=>{div.innerHTML=count
+                count--;
+                setTimeout(()=>{div.innerHTML=count
+                    count--;
+                    setTimeout(()=>{div.innerHTML=count
+                        count--;
+                        setTimeout(()=>{div.innerHTML=count
+                            count--;
+                            setTimeout(()=>{div.innerHTML=count
+                                count--;
+                                setTimeout(()=>{div.innerHTML=content;    
+                                    },1000);
+                                },1000);
+                            },1000);
+                        },1000);
+                    },1000);
+                },1000);
+            },1000);
+        },1000);
+    },1000);
+},1000);
